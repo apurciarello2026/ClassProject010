5818f1195689e3a5ba88f0c852db394e71b45519
Ayan Shahzada <Ayan.Shahzada@Marquette.edu>
2024-05-09 20:53:02 -0500

    Updated patient class

---
+++ Patient.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassProject010;
+
+// Patient class representing a patient
+public class Patient
+{
+    // Properties for patient's first name, last name, username, and password
+    public string PatientFirstName { get; set; }
+    public string PatientLastName { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
